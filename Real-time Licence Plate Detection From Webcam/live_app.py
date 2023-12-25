dd97bdfc686eec11fe335eadf0acb04467819621
Jumale Abdi Jillo <113813239+jamalabdi2@users.noreply.github.com>
2023-12-25 17:11:39 +0900

    Update live_app.py

--- Real-time Licence Plate Detection From Webcam/live_app.py
+++ Real-time Licence Plate Detection From Webcam/live_app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def yolo_detection_img(frame):
     # Load YOLO model
-    net = cv2.dnn.readNet("/Users/jamal/Documents/custom_yolov3/yolov3_training_last.weights", "yolov3_training.cfg")
+    net = cv2.dnn.readNet("yolov3_training_last.weights", "yolov3_training.cfg")
 
     # Name custom object
     classes = ["number_plate"]
