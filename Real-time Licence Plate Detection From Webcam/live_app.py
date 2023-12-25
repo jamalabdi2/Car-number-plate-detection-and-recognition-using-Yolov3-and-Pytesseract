7ddcdb3619497574fad0c16f96329247539c06ad
Jumale Abdi Jillo <113813239+jamalabdi2@users.noreply.github.com>
2023-12-25 17:10:59 +0900

    Add files via upload
    Real time detection from webcam

---
+++ Real-time Licence Plate Detection From Webcam/live_app.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, Response
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+def yolo_detection_img(frame):
+    # Load YOLO model
+    net = cv2.dnn.readNet("/Users/jamal/Documents/custom_yolov3/yolov3_training_last.weights", "yolov3_training.cfg")
+
+    # Name custom object
+    classes = ["number_plate"]
+
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    colors = (255, 0, 0)
+
+    img = cv2.resize(frame, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
+
+    # Detect objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Showing information on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.3:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    # Draw bounding boxes
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            color = colors
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+
+    return img
+
+def webcam_capture():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        processed_frame = yolo_detection_img(frame)
+
+        _, jpeg = cv2.imencode('.jpg', processed_frame)
+        frame_bytes = jpeg.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+    cap.release()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(webcam_capture(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5003)
